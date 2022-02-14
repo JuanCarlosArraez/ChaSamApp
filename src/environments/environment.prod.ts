b25b8fdbdb348a1f614e0d2a70568f8f4650d4c9
juan carlos arraez <juancarlosarraezb@gmail.com>
2022-02-14 06:57:05 +0100

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   host: 'shazam.p.rapidapi.com',
   url: 'https://shazam.p.rapidapi.com/auto-complete',
-  apiKey: '567efa2b88msh3eab3a254525d0ap1dd37ajsn0c544c650da3',
+  apiKey: '919bec602emsh6435a4671a057cep134da1jsned800d1548eb',
 };
