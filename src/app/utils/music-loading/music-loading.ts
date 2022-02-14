9f5d88faeeb1f22be38672fc3bb2696c02d93b2d
juan carlos arraez <juancarlosarraezb@gmail.com>
2022-02-14 06:56:19 +0100

    music loading fix

--- src/app/utils/music-loading/music-loading.ts
+++ src/app/utils/music-loading/music-loading.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       class="ion-text-center"
       style="position: relative; top:80%"
     >
-    <svg width="20px"  height="20px" viewBox="0 0 51 50">
+    <svg width="50px"  height="50px" viewBox="0 0 51 50">
 
 <rect y="0" width="10" height="50"  fill="#ffffff" rx="5px" ry="5px" stroke-linejoin="round">
     <animate attributeName="height" values="50;10;50" begin="0s" dur="1s" repeatCount="indefinite" />
